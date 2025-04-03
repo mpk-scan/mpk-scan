2ee08815b34847fcc59cfcb3c8e182fd65fc848d
jeremy-neale <jneale118@gmail.com>
2025-04-03 00:37:03 -0400

    Commented out print

--- src/storage/s3_manager.py
+++ src/storage/s3_manager.py
@@ -20,7 +20,7 @@ class S3Manager:
             object_name = file_name
         try:
             self.s3_client.upload_file(file_name, self.bucket_name, object_name)
-            print(f"File {file_name} uploaded to {self.bucket_name}/{object_name}")
+            # print(f"File {file_name} uploaded to {self.bucket_name}/{object_name}")
         except NoCredentialsError:
             print("Credentials not available")
 
@@ -29,7 +29,7 @@ class S3Manager:
             file_name = object_name
         try:
             self.s3_client.download_file(self.bucket_name, object_name, file_name)
-            print(f"File {object_name} downloaded from {self.bucket_name} to {file_name}")
+            # print(f"File {object_name} downloaded from {self.bucket_name} to {file_name}")
         except NoCredentialsError:
             print("Credentials not available")
             sys.exit(1)
